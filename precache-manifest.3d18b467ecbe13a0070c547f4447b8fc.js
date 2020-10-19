705eae52f6f5aaa33cef5da31169f44c98c53883
Neha <nehasuthr@gmail.com>
2020-10-19 12:19:35 -0400

    Updates

---
+++ precache-manifest.3d18b467ecbe13a0070c547f4447b8fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78821d774c455c49f919a3584606637c",
+    "url": "/daily-journal-React-Based-App/index.html"
+  },
+  {
+    "revision": "78a9a6ad1cae0df0b98e",
+    "url": "/daily-journal-React-Based-App/static/css/main.ab77e37c.chunk.css"
+  },
+  {
+    "revision": "2444c123f591a8fefca6",
+    "url": "/daily-journal-React-Based-App/static/js/2.58a64fe4.chunk.js"
+  },
+  {
+    "revision": "f459cc55aefe3795c450f29fcffb8a19",
+    "url": "/daily-journal-React-Based-App/static/js/2.58a64fe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a9a6ad1cae0df0b98e",
+    "url": "/daily-journal-React-Based-App/static/js/main.076b9979.chunk.js"
+  },
+  {
+    "revision": "302ab4a032a9899f9e64",
+    "url": "/daily-journal-React-Based-App/static/js/runtime-main.fb9d3d22.js"
+  }
+]);
